9ff2c5c7ad9631b1eb23fb036a4657f38f5a437d
Gordon Watts <gwatts@uw.edu>
2024-09-10 15:43:54 -0700

    Clean up the ciodegen argument, and extra import.

--- template/package/src/sx_dataset.py
+++ template/package/src/sx_dataset.py
@@ -1,4 +1,3 @@
-from typing import Union
 from .event_collection import Event
 
 from servicex.func_adl.func_adl_dataset import FuncADLQuery as sxFuncADLQuery
@@ -13,15 +12,12 @@ class FuncADLQuery{{ calibration_name }}(sxFuncADLQuery[Event]):
 
         Args:
         * `item_type` - The type of this object. Will default to `Event`.
-        * `codegen` - This backend code-generator. Defaults to `{{ backend_default_name }}`.
 
         Note:
         * The current front-end ignores the `codegen` argument.
         '''
         if "item_type" not in kwargs:
             kwargs["item_type"] = Event
-        if "codegen" not in kwargs:
-            kwargs["codegen"] = "{{ backend_default_name }}"
 
         super().__init__(**kwargs)
 
